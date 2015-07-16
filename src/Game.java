92662273f8782f0d185820c79d8ef30c5f24dc11
Daniel <daniel.lopes04@gmail.com>
2015-07-16 12:38:43 -0300

    Tentando adicionar o moviento do mouse

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,14 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.util.ArrayList;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.*;
 //import java.awt.Event;
 
-public class Game {
+public class Game implements MouseMotionListener {
 
 	private World world;
 	private Ball[] greenList;
@@ -17,23 +23,33 @@ public class Game {
 	private Color background = StdDraw.GRAY;
 	private ArrayList<Ball> collidedList;
 	
+	JFrame frame = new JFrame();
+	JPanel telaJogo = new JPanel();
+	
 	public Game() {
 		world = new World(width, height, background);
 		greenList = new Ball[quantGreens];
 		blueList = new Ball[quantBlues];
 		magentaList = new Ball[quantMagentas];
 		
+		//telaJogo.addMouseListener(chief);
+		telaJogo.addMouseMotionListener(this);
+		frame.getContentPane().add(BorderLayout.CENTER, telaJogo);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 500);
+		frame.setVisible(true);
+		
 		collidedList = new ArrayList<Ball>();
 		
 		for (int i = 0; i < quantGreens; i++) {
 			greenList[i] = new Ball(
 								new Point(
-									2 * Math.random() - 1, 
-									Math.random()
+									Math.random(), 
+									0.95
 								), 
 								new Velocity(
-									-0.02 * Math.random(), 
-									0.02 * Math.random()
+									-0.01 * Math.random(), 
+									-0.01 * Math.random()
 								), 
 								0.05,
 								StdDraw.GREEN
@@ -47,8 +63,8 @@ public class Game {
 									Math.random()
 								), 
 								new Velocity(
-									-0.02 * Math.random(), 
-									0.02 * Math.random()
+									-0.01 * Math.random(), 
+									0.01 * Math.random()
 								), 
 								0.05,
 								StdDraw.BLUE
@@ -62,8 +78,8 @@ public class Game {
 									Math.random()
 								), 
 								new Velocity(
-									-0.02 * Math.random(), 
-									0.02 * Math.random()
+									-0.01 * Math.random(), 
+									0.01 * Math.random()
 								), 
 								0.05,
 								StdDraw.MAGENTA
@@ -76,8 +92,8 @@ public class Game {
 					Math.random()
 				), 
 				new Velocity(
-					-0.02 * Math.random(), 
-					0.02 * Math.random()
+					-0.01 * Math.random(), 
+					0.01 * Math.random()
 				), 
 				0.05, 
 				StdDraw.RED
@@ -126,8 +142,33 @@ public class Game {
 				}
 			}
 			
+			/*for (int i = 0; i < quantMagentas; i++) {
+				if (chief.hasCollided(magentaList[i])) {					
+					roda = false;
+					System.out.println("Cabou");
+				}
+			} */
+			
 			StdDraw.show(10);
 		}
+	}
+	
+	public void mouseDragged(MouseEvent e) {}
+	
+	public void mouseMoved(MouseEvent me) {
+		int newX = me.getX();
+		int newY = me.getY();
+		Point newCenter = new Point(newX, newY);
+		
+		if(newY > 2) {
+			newCenter.setPy(2);
+		}
+		
+		if(newX > 2) {
+			newCenter.setPx(2);
+		}
+		
+		chief.setCenter(newCenter);
 	}
 	
 }
