edd43934fe85e31c8975050ade105767086f1248
Daniel <daniel.lopes04@gmail.com>
2015-07-16 10:53:32 -0300

    Adicionamos bolinhas

--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,32 @@
 import java.awt.Color;
 import java.util.ArrayList;
+//import java.awt.Event;
 
 public class Game {
 
 	private World world;
-	private Ball[] targetList;
+	private Ball[] greenList;
+	private Ball[] blueList;
+	private Ball[] magentaList;
 	private Ball chief;
 	private int width = 2;
 	private int height = 2;
-	private int quantTargets = 40;
+	private int quantGreens = 20;
+	private int quantBlues = 10;
+	private int quantMagentas = 40;
 	private Color background = StdDraw.GRAY;
 	private ArrayList<Ball> collidedList;
 	
 	public Game() {
 		world = new World(width, height, background);
-		targetList = new Ball[quantTargets];
+		greenList = new Ball[quantGreens];
+		blueList = new Ball[quantBlues];
+		magentaList = new Ball[quantMagentas];
+		
 		collidedList = new ArrayList<Ball>();
 		
-		for (int i = 0; i < quantTargets; i++) {
-			targetList[i] = new Ball(
+		for (int i = 0; i < quantGreens; i++) {
+			greenList[i] = new Ball(
 								new Point(
 									2 * Math.random() - 1, 
 									Math.random()
@@ -28,22 +36,52 @@ public class Game {
 									0.02 * Math.random()
 								), 
 								0.05,
-								StdDraw.RED
+								StdDraw.GREEN
+							);
+		}
+		
+		for (int i = 0; i < quantBlues; i++) {
+			blueList[i] = new Ball(
+								new Point(
+									2 * Math.random() - 1, 
+									Math.random()
+								), 
+								new Velocity(
+									-0.02 * Math.random(), 
+									0.02 * Math.random()
+								), 
+								0.05,
+								StdDraw.BLUE
+							);
+		}
+		
+		for (int i = 0; i < quantMagentas; i++) {
+			magentaList[i] = new Ball(
+								new Point(
+									2 * Math.random() - 1, 
+									Math.random()
+								), 
+								new Velocity(
+									-0.02 * Math.random(), 
+									0.02 * Math.random()
+								), 
+								0.05,
+								StdDraw.MAGENTA
 							);
 		}
 		
 		chief = new Ball(
-					new Point(
-						2 * Math.random() - 1, 
-						Math.random()
-					), 
-					new Velocity(
-						-0.02 * Math.random(), 
-						0.02 * Math.random()
-					), 
-					0.05, 
-					StdDraw.YELLOW
-				);
+				new Point(
+					2 * Math.random() - 1, 
+					Math.random()
+				), 
+				new Velocity(
+					-0.02 * Math.random(), 
+					0.02 * Math.random()
+				), 
+				0.05, 
+				StdDraw.RED
+			);
 	}
 	
 	public void go() {
@@ -53,21 +91,38 @@ public class Game {
 		while (roda == true) {
 			StdDraw.clear(world.getBackground());
 			
-			for (int i = 0; i < quantTargets; i++) {
-				targetList[i].move(world);
-				targetList[i].draw();
+			for (int i = 0; i < quantGreens; i++) {
+				greenList[i].move(world);
+				greenList[i].draw();
+			}
+			
+			for (int i = 0; i < quantBlues; i++) {
+				blueList[i].move(world);
+				blueList[i].draw();
+			}
+			
+			for (int i = 0; i < quantMagentas; i++) {
+				magentaList[i].move(world);
+				magentaList[i].draw();
 			}
 			
 			chief.move(world);
 			chief.draw();
 			
-			for (int i = 0; i < quantTargets; i++) {
-				if (chief.hasCollided(targetList[i])) {					
-					collidedList.add(targetList[i]);
-					targetList[i].setV(chief.getV());
+			for (int i = 0; i < quantGreens; i++) {
+				if (chief.hasCollided(greenList[i])) {					
+					collidedList.add(greenList[i]);
+					greenList[i].setV(chief.getV());
 				}
 				else{
 					
+				}
+			}
+
+			for (int i = 0; i < quantBlues; i++) {
+				if (chief.hasCollided(blueList[i])) {					
+					collidedList.add(blueList[i]);
+					blueList[i].setV(chief.getV());
 				}
 			}
 			
---
+++ src/Teste.java
@@ -0,0 +1,9 @@
+import java.awt.event.*;
+
+public class Teste implements ActionListener{
+
+	public void actionPerformed(ActionEvent event) {
+		
+	}
+	
+}
